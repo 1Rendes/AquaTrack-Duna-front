1c75d744ed38e96a8e83c6651bf7ee5e24809943
Lyuzika <lyuzika@gmail.com>
2024-11-24 14:51:29 +0200

    signIn add

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,11 +1,18 @@
-import { Link } from "react-router-dom";
+import SignInForm from "../components/SignInForm/SignInForm";
+import AdvantagesSection from "../components/AdvantagesSection/AdvantagesSection";
+import { Toaster } from "react-hot-toast";
 
 const SignInPage = () => {
   return (
-    <div>
-      <p>SignInPage</p>
-      <Link to="/">Go to HomePage</Link>
-    </div>
+    <>
+      <div>
+        <SignInForm />
+      </div>
+      <div>
+        <AdvantagesSection />
+      </div>
+      <Toaster position="top-right" />
+    </>
   );
 };
 
