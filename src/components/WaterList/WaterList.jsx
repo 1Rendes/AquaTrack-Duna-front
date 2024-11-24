d41faeebf5ea7cee148f7ebef647b46e0c806316
Anna <iaanya27@gmail.com>
2024-11-24 19:22:19 +0200

    added WaterList with array of objects filteredWater

--- src/components/WaterList/WaterList.jsx
+++ src/components/WaterList/WaterList.jsx
@@ -1,13 +1,46 @@
+// import { useSelector } from "react-redux";
+import WaterItem from "../WaterItem/WaterItem";
+// import { useState } from "react";
+import AddWaterBtn  from "../AddWaterBtn/AddWaterBtn";
+import css from "./WaterList.module.css";
 
-        import css from './WaterList.module.css';
+const WaterList = ({ filteredWater }) => {
+  //   const filteredWater = useSelector(selectFilteredWater);
+        //   const water = useSelector(selectWater);
+  return (
+    <div className={css.waterlist}>
+      <div className={css.waterlistTitle}>
+        <p className={css.waterlistDay}>Today</p>
 
-        const WaterList = () => {
-        return (
-                <div className={css.waterlist}>
-                {/* Your component code here */}
-                </div>
-                );
-        };
+        <div className={css.waterlistBtn}>
+          {/* <button
+            className={css.waterlistAddbtn}
+            type="button"
+            onClick={AddWaterBtn}
+          > */}
 
-        export default WaterList;
-        
+          {/* <svg className={css.icon}>
+              <use href="/sprite.svg#edit-icon"></use>
+            </svg> */}
+          {/* </button> */}
+          <AddWaterBtn />
+          <span className={css.waterlistBtnText}>Add water</span>
+        </div>
+      </div>
+
+      <ul className={css.waterlistPortions}>
+        {/* {water.length === 0 && (
+            <p>Тo information on water consumption for the selected day</p>
+          )} */}
+
+        {filteredWater.map(({ id, water, time }) => (
+          <li className={css.item} key={id}>
+            <WaterItem water={water} time={time} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default WaterList;
