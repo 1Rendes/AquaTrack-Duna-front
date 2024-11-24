f880092f5537add82d63ce7bfda9a607da9b55e5
Katrin <kateyka1410@gmail.com>
2024-11-24 18:35:58 +0200

    Create Advantages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import WelcomeSection from "/src/components/WelcomeSection/WelcomeSection.jsx";
-  
+import AdvantagesSection from "../components/AdvantagesSection/AdvantagesSection";
 
 const HomePage = () => {
   return (
     <div>
       <WelcomeSection />
+      <AdvantagesSection />
       {/* <p>HomePage</p>
       <Link to="/signin">Go to SignIn Page</Link>
       <Link to="/signup">Go to SignUp Page</Link> */}
