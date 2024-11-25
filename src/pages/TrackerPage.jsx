8e5b144ddd65f9937a8ab63b0f8bebfa737f1918
1Rendes <1admin@ukr.net>
2024-11-25 20:08:43 +0100

    trackePage

--- src/pages/TrackerPage.jsx
+++ src/pages/TrackerPage.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import WaterModal from "../components/WaterModal/WaterModal";
+import css from "./TrackerPage.module.css";
+
 const TrackerPage = () => {
-  return <>TrackerPage</>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalType, setModalType] = useState("");
+  const handleWaterAddOrEditModal = (modalType) => {
+    setModalType(modalType);
+    setIsModalOpen(true);
+  };
+  const handleClosingModal = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <div className={css.trackerpage}>
+      <WaterModal values={modalType}></WaterModal>
+      <button
+        onClick={() => {
+          handleWaterAddOrEditModal("edit");
+        }}
+      ></button>
+      <button
+        onClick={() => {
+          handleWaterAddOrEditModal("add");
+        }}
+      ></button>
+    </div>
+  );
 };
 
 export default TrackerPage;
