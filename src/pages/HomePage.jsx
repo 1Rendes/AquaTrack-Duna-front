59fd8d5428731a9b0a927b0c5410191a3c4e7577
Katrin <kateyka1410@gmail.com>
2024-11-26 22:02:23 +0200

    update sections

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,18 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import WelcomeSection from "/src/components/WelcomeSection/WelcomeSection.jsx";
 import AdvantagesSection from "../components/AdvantagesSection/AdvantagesSection";
+ 
 
 const HomePage = () => {
+
   return (
-    <div>
-      <WelcomeSection />
-      <AdvantagesSection />
-      {/* <p>HomePage</p>
-      <Link to="/signin">Go to SignIn Page</Link>
-      <Link to="/signup">Go to SignUp Page</Link> */}
+    <div >
+      <div>
+        <WelcomeSection />
+      </div>
+      <div>
+        <AdvantagesSection />
+      </div>
     </div>
   );
 };
