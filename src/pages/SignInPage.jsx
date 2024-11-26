37fa7fb85beadc6cca73c46ce239e28891c6791a
Lyuzika <lyuzika@gmail.com>
2024-11-26 11:27:58 +0200

    css fix

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,10 +1,12 @@
 import SignInForm from "../components/SignInForm/SignInForm";
 import AdvantagesSection from "../components/AdvantagesSection/AdvantagesSection";
+import Logo from '../components/Logo/Logo';
 
 const SignInPage = () => {
   return (
     <>
       <div>
+        <Logo />
         <SignInForm />
       </div>
       <div>
