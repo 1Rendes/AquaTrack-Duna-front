8e6ec152600fff2c2a4b506395b3e8560ded1d86
Anna <iaanya27@gmail.com>
2024-11-26 16:35:17 +0200

    added modals to WaterList and WaterItem

--- src/components/WaterItem/WaterItem.jsx
+++ src/components/WaterItem/WaterItem.jsx
@@ -1,55 +1,67 @@
-// import { useDispatch } from "react-redux";
-
-// import { DeleteWater } from "../../redux/water/operations";
-// import { EditWater } from "../../redux/water/operations";
-
+import { useState } from "react";
 import css from "./WaterItem.module.css";
+import Modal from "../Modal/Modal";
+import WaterModal from "../WaterModal/WaterModal";
+import DeleteWaterModal from "../DeleteWaterModal/DeleteWaterModal";
 
-const WaterItem = ({ water, time }) => {
-  // const dispatch = useDispatch();
-  // const handleDeleteWater = () => {
-  //   dispatch(DeleteWater(id));
-  // };
-  // const handleEditWater = () => {
-  //   dispatch(EditWater(id));
-  // };
+const WaterItem = ({ water, time, id }) => {
+  const [editModalIsOpen, setEditIsOpen] = useState(false);
+  const [delModalIsOpen, setDelIsOpen] = useState(false);
+
+  const handleEditClick = () => setEditIsOpen(true);
+  const handleDelClick = () => setDelIsOpen(true);
+  const handleCloseEditModal = () => setEditIsOpen(false);
+  const handleCloseDelModal = () => setDelIsOpen(false);
+
   return (
     <div className={css.wateritem}>
-      {
-        <>
-          <div>
-            <img
-              className={css.img}
-              src='../../img/mob_mage_water-glass-fill_1x.png'
-              alt="A glass of water"
-              // width="38"
-              // height="38"
-            />
-          </div>
-          <ul className={css.infoList}>
-            <li className={css.waterVolume}>{water}</li>
-            <li className={css.infoListTime}>{time}</li>
-          </ul>
-          <ul className={css.btnList}>
-            <li className={css.btnlistItem}>
-              <button className={css.btn} type="button">
-                {/* // onClick={handleEditWater}> */}
-                <svg className={css.icon}>
-                  <use href="/sprite.svg#edit-icon"></use>
-                </svg>
-              </button>
-            </li>
-            <li className={css.btnlistItem}>
-              <button className={css.btn} type="button">
-                {/* // onClick={handleDeleteWater} */}
-                <svg className={css.icon} height="14">
-                  <use href="/sprite.svg#delete-icon"></use>
-                </svg>
-              </button>
-            </li>
-          </ul>
-        </>
-      }
+      <div>
+        <picture>
+          <source
+            srcSet="/img/desk_tab_mage_water-glass-fill_1x.png 1x, /img/desk_tab_mage_water-glass-fill_2x.png 2x"
+            media="(min-width: 768px)"
+          />
+          <source
+            srcSet="/img/mob_mage_water-glass-fill_1x.png 1x, /img/mob_mage_water-glass-fill_2x.png 2x"
+            media="(max-width: 767px)"
+          />
+          <img
+            className={css.img}
+            src="/img/desk_tab_mage_water-glass-fill_1x.png"
+            alt="A glass of water"
+          />
+        </picture>
+      </div>
+      <ul className={css.infoList}>
+        <li className={css.waterVolume}>{water || "No data"}</li>
+        <li className={css.infoListTime}>{time || "No time provided"}</li>
+      </ul>
+      <ul className={css.btnList}>
+        <li className={css.btnlistItem}>
+          <button className={css.btn} type="button" onClick={handleEditClick}>
+            <svg className={css.icon}>
+              <use href="../../img/icons.svg#icon-edit-2"></use>
+            </svg>
+          </button>
+        </li>
+        <li className={css.btnlistItem}>
+          <button className={css.btn} type="button" onClick={handleDelClick}>
+            <svg className={css.icon} height="14">
+              <use href="../../img/icons.svg#icon-drop-down"></use>
+            </svg>
+          </button>
+        </li>
+      </ul>
+      {editModalIsOpen && (
+        <Modal>
+          <WaterModal handleClose={handleCloseEditModal} id={id} />
+        </Modal>
+      )}
+      {delModalIsOpen && (
+        <Modal>
+          <DeleteWaterModal handleClose={handleCloseDelModal} id={id} />
+        </Modal>
+      )}
     </div>
   );
 };
--- src/components/WaterList/WaterList.jsx
+++ src/components/WaterList/WaterList.jsx
@@ -1,44 +1,48 @@
-// import { useSelector } from "react-redux";
+import { selectDayWater } from "../../redux/water/selectors";
+import { useSelector } from "react-redux";
 import WaterItem from "../WaterItem/WaterItem";
 // import { useState } from "react";
-import AddWaterBtn  from "../AddWaterBtn/AddWaterBtn";
+import AddWaterBtn from "../AddWaterBtn/AddWaterBtn";
 import css from "./WaterList.module.css";
+import { useState } from "react";
+import Modal from "../Modal/Modal";
+import WaterModal from "../WaterModal/WaterModal";
 
-const WaterList = ({ filteredWater }) => {
-  //   const filteredWater = useSelector(selectFilteredWater);
-        //   const water = useSelector(selectWater);
+const WaterList = () => {
+  const dayWaterList = useSelector(selectDayWater);
+  const [addWaterModalIsOpen, setAddWaterIsOpen] = useState(false);
+
+  const handleAddWaterClick = () => setAddWaterIsOpen(true);
+   const handleCloseAddWaterModal = () => setAddWaterIsOpen(false);
+  
+ 
   return (
     <div className={css.waterlist}>
       <div className={css.waterlistTitle}>
         <p className={css.waterlistDay}>Today</p>
 
-        <div className={css.waterlistBtn}>
-          {/* <button
-            className={css.waterlistAddbtn}
-            type="button"
-            onClick={AddWaterBtn}
-          > */}
-
-          {/* <svg className={css.icon}>
-              <use href="/sprite.svg#edit-icon"></use>
-            </svg> */}
-          {/* </button> */}
+        <div className={css.waterlistBtn} onClick={handleAddWaterClick}>
           <AddWaterBtn />
           <span className={css.waterlistBtnText}>Add water</span>
         </div>
       </div>
 
       <ul className={css.waterlistPortions}>
-        {/* {water.length === 0 && (
-            <p>Тo information on water consumption for the selected day</p>
-          )} */}
+        {dayWaterList.length === 0 && (
+          <p>No information on water consumption for the selected day</p>
+        )}
 
-        {filteredWater.map(({ id, water, time }) => (
+        {dayWaterList.map(({ id, water, time }) => (
           <li className={css.item} key={id}>
             <WaterItem water={water} time={time} />
           </li>
         ))}
       </ul>
+      {addWaterModalIsOpen && (
+        <Modal>
+          <WaterModal handleClose={handleCloseAddWaterModal}/>
+        </Modal>
+      )}
     </div>
   );
 };
