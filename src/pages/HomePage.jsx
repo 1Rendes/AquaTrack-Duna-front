3bd0e2028a72c3851c218b6d076867b5260af459
Katrin <kateyka1410@gmail.com>
2024-11-27 22:25:05 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,17 @@
 // import { Link } from "react-router-dom";
 import WelcomeSection from "/src/components/WelcomeSection/WelcomeSection.jsx";
 import AdvantagesSection from "../components/AdvantagesSection/AdvantagesSection";
+import HomeWrapper from "../components/Homewraper/HomeWrapper";
 
 const HomePage = () => {
 
   return (
     
     <div>
-      
+      <HomeWrapper>
         <WelcomeSection />
         <AdvantagesSection />
-    
+    </HomeWrapper>
     </div>
   );
 };
