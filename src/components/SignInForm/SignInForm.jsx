4db7568ce44f28d0996a1671516bde6c8b76bd6f
1Rendes <1admin@ukr.net>
2024-11-28 22:03:19 +0100

    css home signin signup fix

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -6,6 +6,7 @@ import css from "./SignInForm.module.css";
 import sprite from "../../img/icons.svg";
 import { login } from "../../redux/auth/operations";
 import { useDispatch } from "react-redux";
+import Logo from "../Logo/Logo";
 
 const initialValues = {
   email: "",
@@ -17,7 +18,7 @@ const validationSchema = Yup.object().shape({
     .email("Invalid email address")
     .required("Email is required"),
   password: Yup.string()
-    .min(6, "Must contain at least 6 characters")
+    .min(5, "Must contain at least 6 characters")
     .required("Password is required"),
 });
 
@@ -46,75 +47,77 @@ const SignInForm = () => {
 
   return (
     <div className={css.wrapper}>
-      <h2 className={css.title}>Sign In</h2>
-      <Formik
-        validationSchema={validationSchema}
-        initialValues={initialValues}
-        onSubmit={handleSubmit}
-      >
-        <Form noValidate autoComplete="off" className={css.form}>
-          <div className={css.field}>
-            <label htmlFor="email" className={css.label}>
-              Email
-            </label>
-            <Field
-              type="text"
-              id="email"
-              name="email"
-              placeholder="Enter your email"
-              className={css.input}
-            />
-            <ErrorMessage
-              className={css.errorMessage}
-              name="email"
-              component="div"
-            />
-          </div>
-
-          <div className={css.field}>
-            <label htmlFor="password" className={css.label}>
-              Password
-            </label>
-            <div className={css.iconWrapper}>
+      <Logo />
+      <div className={css.formWrapper}>
+        <h2 className={css.title}>Sign In</h2>
+        <Formik
+          validationSchema={validationSchema}
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+        >
+          <Form noValidate autoComplete="off" className={css.form}>
+            <div className={css.field}>
+              <label htmlFor="email" className={css.label}>
+                Email
+              </label>
               <Field
-                type={visiblePassword ? "text" : "password"}
-                id="password"
-                name="password"
-                placeholder="Enter your password"
+                type="text"
+                id="email"
+                name="email"
+                placeholder="Enter your email"
                 className={css.input}
               />
-              <svg
-                className={css.icon}
-                width={20}
-                height={20}
-                onClick={() => setVisiblePassword(!visiblePassword)}
-              >
-                <use
-                  href={`${sprite}#${
-                    visiblePassword ? "icon-eye" : "icon-eye-off"
-                  }`}
-                />
-              </svg>
+              <ErrorMessage
+                className={css.errorMessage}
+                name="email"
+                component="div"
+              />
             </div>
-            <ErrorMessage
-              name="password"
-              component="div"
-              className={css.errorMessage}
-            />
-          </div>
-          <div className={css.btnWrapper}>
-            <button type="submit" className={css.btn}>
-              Sign In
-            </button>
-          </div>
-        </Form>
-      </Formik>
 
-      <div className={css.textSignIn}>
-        <p>Don’t have an account?</p>
-        <NavLink className={css.navLink} to="/signup">
-          Sign Up
-        </NavLink>
+            <div className={css.field}>
+              <label htmlFor="password" className={css.label}>
+                Password
+              </label>
+              <div className={css.iconWrapper}>
+                <Field
+                  type={visiblePassword ? "text" : "password"}
+                  id="password"
+                  name="password"
+                  placeholder="Enter your password"
+                  className={css.input}
+                />
+                <svg
+                  className={css.icon}
+                  width={20}
+                  height={20}
+                  onClick={() => setVisiblePassword(!visiblePassword)}
+                >
+                  <use
+                    href={`${sprite}#${
+                      visiblePassword ? "icon-eye" : "icon-eye-off"
+                    }`}
+                  />
+                </svg>
+              </div>
+              <ErrorMessage
+                name="password"
+                component="div"
+                className={css.errorMessage}
+              />
+            </div>
+            <div className={css.btnWrapper}>
+              <button type="submit" className={css.btn}>
+                Sign In
+              </button>
+            </div>
+          </Form>
+        </Formik>
+        <div className={css.textSignIn}>
+          <p>Don’t have an account?</p>
+          <NavLink className={css.navLink} to="/signup">
+            Sign Up
+          </NavLink>
+        </div>
       </div>
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,13 @@
-// import { Link } from "react-router-dom";
 import WelcomeSection from "/src/components/WelcomeSection/WelcomeSection.jsx";
 import AdvantagesSection from "../components/AdvantagesSection/AdvantagesSection";
 import HomeWrapper from "../components/HomeWrapper/HomeWrapper";
 
 const HomePage = () => {
   return (
-    <div >
-      <HomeWrapper>
-        <WelcomeSection />
-        <AdvantagesSection />
-      </HomeWrapper>
-    </div>
+    <HomeWrapper>
+      <WelcomeSection />
+      <AdvantagesSection />
+    </HomeWrapper>
   );
 };
 
