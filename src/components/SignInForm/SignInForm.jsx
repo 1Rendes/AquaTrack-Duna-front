56e0361a773dd0e396b67c720bb9026905a9429a
Maksym Kosenko <maksym.kosenko2014@gmail.com>
2024-11-29 13:55:58 +0200

    invalid email password btn disabled

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -7,6 +7,7 @@ import sprite from "../../img/icons.svg";
 import { login } from "../../redux/auth/operations";
 import { useDispatch } from "react-redux";
 import Logo from "../Logo/Logo";
+import clsx from "clsx";
 
 const initialValues = {
   email: "",
@@ -55,62 +56,84 @@ const SignInForm = () => {
           initialValues={initialValues}
           onSubmit={handleSubmit}
         >
-          <Form noValidate autoComplete="off" className={css.form}>
-            <div className={css.field}>
-              <label htmlFor="email" className={css.label}>
-                Email
-              </label>
-              <Field
-                type="text"
-                id="email"
-                name="email"
-                placeholder="Enter your email"
-                className={css.input}
-              />
-              <ErrorMessage
-                className={css.errorMessage}
-                name="email"
-                component="div"
-              />
-            </div>
-
-            <div className={css.field}>
-              <label htmlFor="password" className={css.label}>
-                Password
-              </label>
-              <div className={css.iconWrapper}>
-                <Field
-                  type={visiblePassword ? "text" : "password"}
-                  id="password"
-                  name="password"
-                  placeholder="Enter your password"
-                  className={css.input}
+          {({ isValid, dirty }) => (
+            <Form noValidate autoComplete="off" className={css.form}>
+              <div className={css.field}>
+                <label htmlFor="email" className={css.label}>
+                  Email
+                </label>
+                <Field name="email">
+                  {({ field, meta }) => (
+                    <input
+                      {...field}
+                      type="text"
+                      id="email"
+                      placeholder="Enter your email"
+                      className={clsx(css.input, {
+                        [css.inputError]: meta.touched && meta.error,
+                      })}
+                    />
+                  )}
+                </Field>
+                <ErrorMessage
+                  className={css.errorMessage}
+                  name="email"
+                  component="div"
                 />
-                <svg
-                  className={css.icon}
-                  width={20}
-                  height={20}
-                  onClick={() => setVisiblePassword(!visiblePassword)}
-                >
-                  <use
-                    href={`${sprite}#${
-                      visiblePassword ? "icon-eye" : "icon-eye-off"
-                    }`}
-                  />
-                </svg>
               </div>
-              <ErrorMessage
-                name="password"
-                component="div"
-                className={css.errorMessage}
-              />
-            </div>
-            <div className={css.btnWrapper}>
-              <button type="submit" className={css.btn}>
-                Sign In
-              </button>
-            </div>
-          </Form>
+
+              <div className={css.field}>
+                <label htmlFor="password" className={css.label}>
+                  Password
+                </label>
+                <div className={css.iconWrapper}>
+                  <Field name="password">
+                    {({ field, meta }) => (
+                      <div className={css.iconWrapper}>
+                        <input
+                          {...field}
+                          type={visiblePassword ? "text" : "password"}
+                          id="password"
+                          placeholder="Enter your password"
+                          className={clsx(css.input, {
+                            [css.inputError]: meta.touched && meta.error,
+                          })}
+                        />
+                        <svg
+                          className={css.icon}
+                          width={20}
+                          height={20}
+                          onClick={() => setVisiblePassword(!visiblePassword)}
+                        >
+                          <use
+                            href={`${sprite}#${
+                              visiblePassword ? "icon-eye" : "icon-eye-off"
+                            }`}
+                          />
+                        </svg>
+                      </div>
+                    )}
+                  </Field>
+                </div>
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className={css.errorMessage}
+                />
+              </div>
+              <div className={css.btnWrapper}>
+                <button
+                  type="submit"
+                  className={clsx(css.btn, {
+                    [css.btnDisabled]: !isValid || !dirty,
+                  })}
+                  disabled={!isValid || !dirty}
+                >
+                  Sign In
+                </button>
+              </div>
+            </Form>
+          )}
         </Formik>
         <div className={css.textSignIn}>
           <p>Don’t have an account?</p>
