6dfb541da7fbb227c64970cbaa01cbfa92ee2520
Nataliia Babina <157317342+Nataliia-Babina@users.noreply.github.com>
2024-11-30 11:07:33 +0100

    deleteModal

--- src/components/DeleteWaterModal/DeleteWaterModal.jsx
+++ src/components/DeleteWaterModal/DeleteWaterModal.jsx
@@ -1,13 +1,39 @@
+import { useDispatch } from 'react-redux';
+import Modal from '../Modal/Modal';
+import css from './DeleteWaterModal.module.css';
+import { deleteWater } from '../../redux/water/operations';
+import CancelButton from '../CancelButton/CancelButton';
 
-        import css from './DeleteWaterModal.module.css';
+const DeleteWaterModal = ({ handleCloseDelModal, delModalIsOpen }) => {
+  const dispatch = useDispatch();
+  
+// const [isLoading, setIsLoading] = useState(false);
+// const dayWaterArray = useSelector(selectDayWater);
+// const waterPortion = dayWaterArray.find(({ _id }) => _id === id);        
+        
 
-        const DeleteWaterModal = () => {
-        return (
-                <div className={css.deletewatermodal}>
-                {/* Your component code here */}
-                </div>
-                );
-        };
+  return (
+    <Modal
+      handleClose={handleCloseDelModal}
+      isOpen={delModalIsOpen}
+      modalType="DeleteEntry"
+    >
+      <h2 className={css.titleOfModal}>Delete entry</h2>
+     
+      <p className={css.description}>Are you sure you want to delete the entry?</p>
+      <div className={css.btnsBox}>
+        <button
+          className={css.logOutBtn}
+          type="button"
+          onClick={() => dispatch(deleteWater())}
+        >
+          Delete entry
+        </button>
+        <CancelButton handleFunc={handleCloseDelModal} />
+        </div>
+    </Modal>
+  );
+};
 
-        export default DeleteWaterModal;
+export default DeleteWaterModal;
         
