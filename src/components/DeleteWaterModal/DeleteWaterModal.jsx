acbcb627fb835fe5d524fe346369c2b875e6a074
Nataliia Babina <157317342+Nataliia-Babina@users.noreply.github.com>
2024-11-30 11:17:39 +0100

    changes

--- src/components/DeleteWaterModal/DeleteWaterModal.jsx
+++ src/components/DeleteWaterModal/DeleteWaterModal.jsx
@@ -1,42 +1,42 @@
 
-import { useDispatch } from 'react-redux';
-import Modal from '../Modal/Modal';
-import css from './DeleteWaterModal.module.css';
-import { deleteWater } from '../../redux/water/operations';
-import CancelButton from '../CancelButton/CancelButton';
+// import { useDispatch } from 'react-redux';
+// import Modal from '../Modal/Modal';
+// import css from './DeleteWaterModal.module.css';
+// import { deleteWater } from '../../redux/water/operations';
+// import CancelButton from '../CancelButton/CancelButton';
 
-const DeleteWaterModal = ({ handleCloseDelModal, delModalIsOpen }) => {
-  const dispatch = useDispatch();
+// const DeleteWaterModal = ({ handleCloseDelModal, delModalIsOpen }) => {
+//   const dispatch = useDispatch();
   
-// const [isLoading, setIsLoading] = useState(false);
-// const dayWaterArray = useSelector(selectDayWater);
-// const waterPortion = dayWaterArray.find(({ _id }) => _id === id);        
+// // const [isLoading, setIsLoading] = useState(false);
+// // const dayWaterArray = useSelector(selectDayWater);
+// // const waterPortion = dayWaterArray.find(({ _id }) => _id === id);        
         
 
-  return (
-    <Modal
-      handleClose={handleCloseDelModal}
-      isOpen={delModalIsOpen}
-      modalType="DeleteEntry"
-    >
-      <h2 className={css.titleOfModal}>Delete entry</h2>
+//   return (
+//     <Modal
+//       handleClose={handleCloseDelModal}
+//       isOpen={delModalIsOpen}
+//       modalType="DeleteEntry"
+//     >
+//       <h2 className={css.titleOfModal}>Delete entry</h2>
      
-      <p className={css.description}>Are you sure you want to delete the entry?</p>
-      <div className={css.btnsBox}>
-        <button
-          className={css.logOutBtn}
-          type="button"
-          onClick={() => dispatch(deleteWater())}
-        >
-          Delete entry
-        </button>
-        <CancelButton handleFunc={handleCloseDelModal} />
-        </div>
-    </Modal>
-  );
-};
+//       <p className={css.description}>Are you sure you want to delete the entry?</p>
+//       <div className={css.btnsBox}>
+//         <button
+//           className={css.logOutBtn}
+//           type="button"
+//           onClick={() => dispatch(deleteWater())}
+//         >
+//           Delete entry
+//         </button>
+//         <CancelButton handleFunc={handleCloseDelModal} />
+//         </div>
+//     </Modal>
+//   );
+// };
 
-export default DeleteWaterModal;
+// export default DeleteWaterModal;
         
 
 
